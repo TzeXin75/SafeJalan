6bf08555b367094002bf1a5c068f3ba56a59c550
Tze <146824048+TzeXin75@users.noreply.github.com>
2026-08-27 02:35:55 +0800

    update

--- lib/widgets/common.dart
+++ lib/widgets/common.dart
@@ -2,11 +2,10 @@ import 'dart:io';
 import 'package:flutter/material.dart';
 import '../models/report.dart';
 
-const navy = Color(0xFF0C2745);
-const primary = Color(0xFF174A76);
-const safeOrange = Color(0xFFFF9F1C);
+const navy = Color(0xFF101828);
+const primary = Color(0xFF3B5BDB);
 const safeTeal = Color(0xFF12B886);
-const safeBg = Color(0xFFF3F7FB);
+const safeBg = Color(0xFFF4F7FC);
 const mutedText = Color(0xFF667085);
 const softBorder = Color(0xFFE4E7EC);
 
@@ -41,20 +40,8 @@ class ReportTile extends StatelessWidget {
   final VoidCallback onTap;
   const ReportTile({super.key, required this.report, required this.onTap});
   @override
-  Widget build(BuildContext context) => Container(
-    margin: const EdgeInsets.only(bottom: 14),
-    decoration: BoxDecoration(
-      color: Colors.white,
-      borderRadius: BorderRadius.circular(22),
-      border: Border.all(color: softBorder.withValues(alpha: .8)),
-      boxShadow: const [
-        BoxShadow(
-          color: Color(0x0D0C2745),
-          blurRadius: 22,
-          offset: Offset(0, 8),
-        ),
-      ],
-    ),
+  Widget build(BuildContext context) => Card(
+    margin: const EdgeInsets.only(bottom: 12),
     child: InkWell(
       borderRadius: BorderRadius.circular(20),
       onTap: onTap,
@@ -126,8 +113,6 @@ class ReportTile extends StatelessWidget {
                 ],
               ),
             ),
-            const SizedBox(width: 6),
-            const Icon(Icons.chevron_right_rounded, color: Color(0xFF98A2B3)),
           ],
         ),
       ),
@@ -178,19 +163,40 @@ class SafeLogo extends StatelessWidget {
   );
 }
 
-class SafeMark extends StatelessWidget {
+class HeroBrandMark extends StatelessWidget {
   final double size;
-  const SafeMark({super.key, this.size = 44});
+  const HeroBrandMark({super.key, this.size = 148});
 
   @override
-  Widget build(BuildContext context) => ClipRRect(
-    borderRadius: BorderRadius.circular(size * .26),
+  Widget build(BuildContext context) => Container(
+    width: size,
+    height: size,
+    padding: EdgeInsets.all(size * .12),
+    decoration: BoxDecoration(
+      shape: BoxShape.circle,
+      gradient: const LinearGradient(
+        begin: Alignment.topLeft,
+        end: Alignment.bottomRight,
+        colors: [Color(0xFFFFFFFF), Color(0xFFEAF2FA)],
+      ),
+      border: Border.all(color: Colors.white, width: 2),
+      boxShadow: [
+        BoxShadow(
+          color: const Color(0xFFFF9F1C).withValues(alpha: .28),
+          blurRadius: 34,
+          spreadRadius: 2,
+        ),
+        const BoxShadow(
+          color: Color(0x33000000),
+          blurRadius: 20,
+          offset: Offset(0, 12),
+        ),
+      ],
+    ),
     child: Image.asset(
       'assets/images/safejalan_app_icon.png',
-      width: size,
-      height: size,
-      fit: BoxFit.cover,
-      semanticLabel: 'SafeJalan app icon',
+      fit: BoxFit.contain,
+      semanticLabel: 'SafeJalan symbol',
     ),
   );
 }
@@ -204,15 +210,6 @@ class PageTitle extends StatelessWidget {
   @override
   Widget build(BuildContext context) => Row(
     children: [
-      Container(
-        width: 4,
-        height: 42,
-        decoration: BoxDecoration(
-          color: safeOrange,
-          borderRadius: BorderRadius.circular(99),
-        ),
-      ),
-      const SizedBox(width: 12),
       Expanded(
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
